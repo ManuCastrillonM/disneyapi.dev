e22a94119c39ff3d9dd06f9d865cd81583f8aeee
manucastrillonm <manu.cm43@gmail.com>
2020-06-07 21:48:23 -0500

    add metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Disney API`,
+    description: `The Disney API is a rest API based on the Disney characters`,
+    author: `@manucastrillonm`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
