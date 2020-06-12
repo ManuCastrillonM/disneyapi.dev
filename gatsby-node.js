df46a78b30fe1c51837ca4936794d7533e3ea56f
manucastrillonm <manu.cm43@gmail.com>
2020-06-11 22:07:46 -0500

    remove console logs

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,4 @@
 exports.createPages = async ({ actions, graphql, reporter }) => {
-  console.log("ONCREATEPAGES");
-
   const { createPage } = actions
 
   const pageTemplate = require.resolve(`./src/templates/markdownTemplate.js`)
@@ -25,12 +23,8 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
   // Handle errors
   if (result.errors) {
     reporter.panicOnBuild(`Error while running GraphQL query.`)
-    console.log("MARKDOWN PAGES ERROR: ", result.errors );
     return
   }
-
-  console.log("MARKDOWN PAGES: ", result.data.allMarkdownRemark.edges );
-
 
   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
     createPage({
