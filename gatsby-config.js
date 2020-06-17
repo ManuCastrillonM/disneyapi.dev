f6e6e1eaa0019793a0e977f1f0514ad26128b82b
manucastrillonm <manu.cm43@gmail.com>
2020-06-17 14:24:33 -0500

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,17 @@ module.exports = {
           }
         ],
       },
+    }, {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-93858303-4",
+        head: false,
+        anonymize: true,
+        respectDNT: true,
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        pageTransitionDelay: 0,
+        defer: false
+      },
     }
   ],
   pathPrefix: "/",
