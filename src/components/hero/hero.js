3215fd6138ca458888ba9c60488500037dc5dbf9
manuCastrillonM <manu.cm43@gmail.com>
2023-04-11 16:31:25 +0200

    Add deprecation note on home page

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import * as styles from './hero.module.css'
+import { Link } from 'gatsby'
 
 const Hero = () => (
   <div className={styles.hero}>
     <div className="container">
       <h1 className={styles.heroTitle}>Disney API</h1>
       <h2>The REST and GraphQL API based on Disney characters</h2>
+    </div>
+    <div className={styles.alert}>
+      <p>
+        Important: The <i>/characters</i> endpoint will be deprecated on
+        2023-04-30. Please use the <i>/character</i> endpoint instead.
+      </p>
+      <p>
+        Checkout the <Link to="/docs">documentation</Link> to learn how to use
+        it.
+      </p>
     </div>
   </div>
 )
