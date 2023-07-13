ec38cb46df238b3e069ad19bdabe4d321439c9bf
manuCastrillonM <manu.cm43@gmail.com>
2023-07-13 12:45:13 +0200

    Delete deprecation note

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -8,16 +8,6 @@ const Hero = () => (
       <h1 className={styles.heroTitle}>Disney API</h1>
       <h2>The REST and GraphQL API based on Disney characters</h2>
     </div>
-    <div className={styles.alert}>
-      <p>
-        Important: The <i>/characters</i> endpoint will be deprecated on
-        2023-04-30. Please use <i>/character</i> instead.
-      </p>
-      <p>
-        Checkout the <Link to="/docs">documentation</Link> to learn how to use
-        it.
-      </p>
-    </div>
   </div>
 )
 
